4e7d57ac29675d3f2fe3ae0f42a86ffe165bfa38
Joel Henrique Silva Santos <joelhenrique2000@gmail.com>
2020-10-13 23:56:09 -0300

    base

--- index.py
+++ index.py
@@ -1,6 +1,6 @@
 import json, requests
 
-def estatisticas(pais):
+def getEstatisticas(pais):
   url = "https://covid-193.p.rapidapi.com/statistics?country="+pais
   headers = {
     'x-rapidapi-host': "covid-193.p.rapidapi.com",
@@ -10,7 +10,7 @@ def estatisticas(pais):
   jsonData = response.json()
   return jsonData["response"][0]
 
-def paises():
+def getPaises():
   url = "https://covid-193.p.rapidapi.com/countries"
   headers = {
     'x-rapidapi-host': "covid-193.p.rapidapi.com",
@@ -18,22 +18,85 @@ def paises():
   }
   response = requests.request("GET", url, headers=headers)
   jsonData = response.json()
-  return jsonData["response"])
+  return jsonData["response"]
+
+def printEstatisticas(pais):
+  estatistica = getEstatisticas(pais)
+
+  print('\n\n\n\n\n\n__________________________________\n')
+  print('-> Continente:.....', estatistica["continent"])
+  print('-> País:...........', estatistica["country"])
+  print('-> População:......', estatistica["population"], ' Habitantes')
+  print('-> Casos:          ')
+  print('   -> Novos:.......', estatistica["cases"]["new"])
+  print('   -> Ativos:......', estatistica["cases"]["active"])
+  print('   -> Críticos:....', estatistica["cases"]["critical"])
+  print('   -> Recuperados:.', estatistica["cases"]["recovered"])
+  print('   -> Total:.......', estatistica["cases"]["total"])
+  print('-> Mortes:     ')
+  print('   -> Novos:.......', estatistica["deaths"]["new"])
+  print('   -> Total:.......', estatistica["deaths"]["total"])
+  print('-> Testes:         ')
+  print('   -> Total:.......', estatistica["tests"]["total"])
+  print('-> Data:...........', estatistica["time"])
+  print('\n__________________________________\n\n\n\n\n\n')
+
+def printPaises():
+  paises = getPaises()
+
+  print('\n\n\n\n\n\n__________________________________\n')
+  for pais in paises:
+    print(pais)
+    
+  print('\n__________________________________\n\n\n\n\n\n')
+
+def salvarInfoTxt(pais):
+
+  # Objeto com as estatisticas do pais
+  estatistica = getEstatisticas(pais)
+
+  # Informações do pais
+  continente = estatistica["continent"]
+  pais = estatistica["country"]
+  populacao = estatistica["population"]
+  
+  # Informações do caso
+  casosNovos = estatistica["cases"]["new"]
+  casosAtivos = estatistica["cases"]["active"]
+  casosCriticos = estatistica["cases"]["critical"]
+  casosRecuperados = estatistica["cases"]["recovered"]
+  casosTotal = estatistica["cases"]["total"]
+
+  # Informações de morte
+  mortesNovos = estatistica["deaths"]["new"])
+  mortesTotal = estatistica["deaths"]["total"])
+
+  # Informações de testes
+  testesTotal = estatistica["tests"]["total"])
+
+  # Informação da data
+  data = estatistica["time"])
+
+  # Aqui vai fazer a lógica para imprimir em um txt
 
 def init():
+  printEstatisticas("brazil")
+  printPaises()
 
-  paises = paises()
+  # Aqui vai ter o menu
+  # Ele sempre vai está rodando, vai ser apresentado as opções
+  # para o usuário. Elas são:
+  # 1. Ver paises (vai imprimir paises)
+  # 2. Ver estatistica de um pais (vai peguntar qual pais é e imprimir)
+  # 3. Salvar estatisticas de um pais no arquivo txt (vai perguntar qual pais)
+  # 4. Sair (Se o usuário quiser sair vai fechar o programa)
 
-  estatistica = estatisticas("brazil")
+  # A dica é usar uma variavel de parada, se ele for True é pq o programa
+  # precisa continuar rodando, caso não o programa fecha
+  # Para isso o While vai ser util, sua condição é a variavel de parada 
+  # é a variavel de parada.
 
-  print(estatistica["continent"])
-  print(estatistica["country"])
-  print(estatistica["population"])
-  print(estatistica["cases"])
-  print(estatistica["deaths"])
-  print(estatistica["tests"])
-  print(estatistica["time"])
-
+# Iniciando a função
 init()
 
 
